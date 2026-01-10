1b6e5ab7266b51816ebc0b24c815845e1a780a5a
shizaterrorblade <shizaterrorblayde@gmail.com>
2026-01-10 16:14:48 +0300

    Update build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -31,3 +31,77 @@ do
       "$EGG_DIR"
     )
   ");
+
+  # Вычисляем URL обновления, если задана переменная URL
+  if [ -n "$URL" ]; then
+    UPDATE_URL="${URL%/}/$URL_PATH/$OUT_NAME";
+  fi
+
+  # Преобразуем конфиг в формат Pterodactyl и сохраняем файл
+  echo "$CONFIG" | jq \
+  "$(
+    printf '
+      def nullable: if . == "" or . == "null" or . == null then null else . end;
+      def default($default): if (. | nullable) == null then $default else . end;
+
+      {
+        _comment: ("%s" | nullable),
+        meta: {
+          version: "PTDL_v2",
+          update_url: ("%s" | nullable)
+        },
+        exported_at: "%s",
+        name: (.name // "" | tostring),
+        description: (.description | tostring | nullable),
+        author: ("%s" | default("admin@faithnode.com")),
+        features: (.features // []),
+        docker_images: (
+          (.images // []) | map(
+            .name as $name |
+            .image as $image |
+            (.tags // [null])[] as $tag | {
+              name: (.name | sub("{tag}"; $tag | tostring)),
+              image: (.image | sub("{tag}"; $tag | tostring))
+            }) | reduce .[] as $i ({}; .[$i.name] = $i.image)
+        ),
+        file_denylist: (.config.denylist // []),
+        startup: (.config.cmd // "echo \\"Hello world\\""),
+        config: {
+          files: (.config.files // {} | tostring),
+          startup: (.config.startup // {"done":""} | tostring),
+          logs: (.config.logs // {} | tostring),
+          stop: (.config.stop // "^c" | tostring),
+        },
+        scripts: {
+          installation: (
+            (.install.container // "debian:bookworm-slim") as $container |
+            (.install.entrypoint // "/bin/bash") as $entrypoint |
+            (.install.script | if ((. // "") | type) == "string" then . = [.] else . end) as $script |
+            {
+              script: ($script | join("\n")),
+              container: $container,
+              entrypoint: $entrypoint,
+            }
+          )
+        },
+        variables: (
+          (.variables // []) | map({
+            name: (.name | tostring),
+            description: (.description | tostring),
+            env_variable: (.env | tostring),
+            default_value: (.value | tostring),
+            user_viewable: (.permissions.view // true | tostring | test("true")),
+            user_editable: (.permissions.edit // true | tostring | test("true")),
+            rules: (.rules // "string" | tostring),
+            field_type: (.type // "text" | tostring)
+          })
+        ),
+      }
+    ' \
+    "$COMMENT" \
+    "$UPDATE_URL" \
+    "$(date '+%Y-%m-%dT%H:%M:%S%:z')" \
+    "$AUTHOR"
+  )" > "${OUT_DIR}/${OUT_NAME}";
+
+done < <(find "$ROOT/eggs" -name 'egg.yml' -print0);
